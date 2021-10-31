a6b51928567bb63fec9de552c1a13b6437671483
leonardo <leonardolopesalves20@gmail.com>
2021-10-31 12:31:29 -0300

    Adicionando o CRUD

---
+++ Data/ComercioLPContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ComercioLP.Models;
+
+namespace ComercioLP.Data
+{
+    public class ComercioLPContext : DbContext
+    {
+        public ComercioLPContext (DbContextOptions<ComercioLPContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ComercioLP.Models.Departament> Departament { get; set; }
+    }
+}
